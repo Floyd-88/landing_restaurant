da0a52c7ecacd56bccb95af93551af9159b664f2
Dio Eracler <dioeracler1988@gmail.com>
2024-09-30 16:38:47 +0300

    slider review

--- slider2_home.js
+++ slider2_home.js
@@ -1,11 +1,12 @@
-let swiperInstance = null;
-
-swiperInstance = new Swiper(".swiper-container", {
+new Swiper(".swiper-container-review", {
   loop: true, // Бесконечная прокрутка
   slidesPerView: 1, // Одна карточка на экране
   spaceBetween: 20, // Отступы между карточками
   centeredSlides: false, // Отключаем центрирование слайда
-  pagination: false, // Убираем точки
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Делаем точки кликабельными
+  },
   navigation: false, // Убираем стрелки
   speed: 400, // Скорость анимации перехода (увеличена для плавности)
   slideToClickedSlide: true, // Переход к слайду по клику
