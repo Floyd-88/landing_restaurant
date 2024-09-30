dd0876d20ed2a847782f0779554928067f9e0b8a
Dio Eracler <dioeracler1988@gmail.com>
2024-09-30 20:54:28 +0300

    refact slider 1

--- slider1_home.js
+++ slider1_home.js
@@ -1,12 +1,29 @@
 let swiperInstance = null;
 
-swiperInstance = new Swiper(".swiper-container", {
-  loop: true, // Бесконечная прокрутка
-  slidesPerView: 1, // Одна карточка на экране
-  spaceBetween: 20, // Отступы между карточками
-  centeredSlides: false, // Отключаем центрирование слайда
-  pagination: false, // Убираем точки
-  navigation: false, // Убираем стрелки
-  speed: 400, // Скорость анимации перехода (увеличена для плавности)
-  slideToClickedSlide: true, // Переход к слайду по клику
+function initSwiper() {
+  if (window.innerWidth <= 760 && !swiperInstance) {
+    // Инициализация слайдера, если ширина меньше или равна 760px
+    swiperInstance = new Swiper(".swiper-container", {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      centeredSlides: false,
+      pagination: false,
+      navigation: false,
+      speed: 400,
+      slideToClickedSlide: true,
+    });
+  } else if (window.innerWidth > 760 && swiperInstance) {
+    // Уничтожение слайдера, если ширина больше 760px
+    swiperInstance.destroy(true, true);
+    swiperInstance = null;
+  }
+}
+
+// Вызов функции при загрузке страницы
+initSwiper();
+
+// Добавление обработчика на событие изменения размера экрана
+window.addEventListener("resize", function () {
+  initSwiper();
 });
